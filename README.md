c01047e9fb39c9aa933fae84e15d405fb28ea4c7
Chan Pham <iamchanpham@gmail.com>
2024-09-16 01:56:45 +0000

    Practice template

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# nested-CRUD-practice
+# Nested CRUD Practice
+These exercises will help you practice the competencies in tomorrow's lab. 
+Start with the section you feel least confident about and complete them all to feel prepared!
+Commit your code between every section and submit by pushing at the end!
---
+++ index.js
@@ -0,0 +1,72 @@
+// These exercises will help you practice for the lab next class
+// FOR LOOPS
+// Task 1
+// Create a for loop that runs 5 times
+// Inside the loop, log the current number
+// After the loop ends, log "Loop Complete"
+
+
+// Task 2
+let sum = 0
+// Create a for loop that runs 3 times
+// Inside the loop, prompt the user to enter a number 
+// Cast it to a number and add the number to sum
+
+
+// Console log "The sum is [sum]"
+
+// Commit changes this section with an appropriate commit message
+// -----------------
+// ARRAYS
+// Task 3
+// Create an array called readingList and enter at least 2 books you want to read
+
+// Prompt the user to ask for a book recommendation
+
+// Append the book to the readingList
+
+// Task 4
+// Modify the second element to be "Beginning NodeJS"
+
+// Delete the first element of the array
+
+// Commit changes this section with an appropriate commit message
+// -----------------
+// OBJECTS
+// Task 5: Create a book object that has a title, author, and pages
+
+
+// Task 6
+// Add a property hasRead to the book object and set it to false
+
+// Prompt the user to ask if they have read the book
+
+// Add an if/else statement such that if the user read the book, hasRead is set to true
+
+// Console log object
+
+// Commit changes this section with an appropriate commit message
+// -----------------
+// NESTED ARRAYS & OBJECTS
+// Task 7
+// Create an empty array called teachers
+
+// Create an object for msPham and mrCastro that has name, height, and funFact
+
+// Add both to the teachers array
+
+// Modify the teachers array to make Ms. Pham's height 6ft
+
+// Delete the first teacher in the array
+
+// Task 8
+let seatingChart = [
+    ["Corey", "Osiris", "John", "Monica"],
+    ["Zyaire", "Miriam", "Jamari", "Alyma"]
+]
+
+// Create an array with 4 students names and add it to the end of the seatingChart array
+
+// Modify "John" to be "John A." through the seatingChart array
+
+// Remove "Alyma" from the seatingChart array
